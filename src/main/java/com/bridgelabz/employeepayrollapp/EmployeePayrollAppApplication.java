5a96c1562f794a94b9a20b4fd1b5453318daa580
unknown <shreyanshg245@gmail.com>
2025-02-27 11:46:55 +0530

    UC-1-Setup employee payroll spring project dont

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,13 +1,11 @@
-package com.bridgelabz.EmployeePayrollApp;
+package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		SpringApplication.run(EmployeePayrollAppApplication.class,args);
 	}
-
 }
--- src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplicationTests.java
+++ src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.EmployeePayrollApp;
+package com.bridgelabz.employeepayrollapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
