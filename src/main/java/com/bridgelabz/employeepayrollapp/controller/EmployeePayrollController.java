fd2047774ed5db10a365a03f6f2da81e83792da2
unknown <shreyanshg245@gmail.com>
2025-02-27 13:22:00 +0530

    Shreyansh add. Exception handling and validation, dependency configuration

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -2,9 +2,11 @@ package com.bridgelabz.employeepayrollapp.controller;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
+import com.bridgelabz.employeepayrollapp.service.EmployeePayrollService;
 import com.bridgelabz.employeepayrollapp.service.IEmployeePayrollService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +18,42 @@ public class EmployeePayrollController {
     @Autowired
     private IEmployeePayrollService employeePayrollService;
 
+    // Get All Employees
+    @GetMapping("/")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employeeList = employeePayrollService.getAllEmployees();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    // Get Employee by ID
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
+        Employee employee = employeePayrollService.getEmployeeById(id);
+        if (employee != null) {
+            return new ResponseEntity<>(employee, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Create New Employee
     @PostMapping("/create")
     public ResponseEntity<Employee> createEmployee(@Valid @RequestBody EmployeeDTO employeeDTO) {
         return ResponseEntity.ok(employeePayrollService.createEmployee(employeeDTO));
     }
 
+    // Update Employee by ID
     @PutMapping("/update/{id}")
     public ResponseEntity<Employee> updateEmployee(
             @PathVariable int id,
             @Valid @RequestBody EmployeeDTO employeeDTO) {
         return ResponseEntity.ok(employeePayrollService.updateEmployee(id, employeeDTO));
+    }
+
+
+    // Delete Employee by ID
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
+        employeePayrollService.deleteEmployee(id);
+        return new ResponseEntity<>("Employee Deleted", HttpStatus.OK);
     }
 }
